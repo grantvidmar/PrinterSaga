bb08dbb76ad890cc55c3438980c9de42042a6d8f
grantvidmar <83886133+grantvidmar@users.noreply.github.com>
2024-04-23 09:23:43 -0500

    Add files via upload
    PS Script to find printers ip and name by requesting it form a print server.

---
+++ printershares.ps1
@@ -0,0 +1,39 @@
+# Define an array of network shares where printers are located
+$networkShares = @(
+    "\\CCTORPRN100",
+    "\\CCYYCPRN100",
+    "\\ccatlprn100",
+    "\\CCRIOPRN100",
+    "\\CCWSHCSS100",
+    "\\CCYVRPRN100"
+
+)
+
+# Define the path where you want to save the CSV file
+$csvFilePath = "C:\temp\printers.csv"
+
+# Initialize an array to store printer information
+$printerInfo = @()
+
+# Loop through each network share
+foreach ($networkShare in $networkShares) {
+    # Get the list of printers from the network share
+    $printers = Get-Printer -ComputerName $networkShare
+    
+    # If there are printers, add their information to the array
+    if ($printers) {
+        foreach ($printer in $printers) {
+            $printerInfo += [PSCustomObject]@{
+                "NetworkShare" = $networkShare
+                "Name" = $printer.Name
+                "PortName" = $printer.PortName
+                "DriverName" = $printer.DriverName
+            }
+        }
+    }
+}
+
+# Export the printer information to a CSV file
+$printerInfo | Export-Csv -Path $csvFilePath -NoTypeInformation
+
+Write-Host "Printer information saved to: $csvFilePath"
